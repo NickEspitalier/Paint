81428565b4742da22fa14f382da43af904747d1b
GenkaZero <dominic-achkarian@outlook.com>
2023-11-05 12:55:07 -0500

    test

--- modeles/ModeleApplication.java
+++ modeles/ModeleApplication.java
@@ -1,5 +1,8 @@
 package modeles;
 
-public class ModeleApplication {
+import java.awt.*;
+import java.util.ArrayList;
 
+public class ModeleApplication {
+    ArrayList<Image> images = new ArrayList<>(3);
 }
