b46a3a4c09e5f85e53e7aed94df6964f070e44f8
GenkaZero <dominic-achkarian@outlook.com>
2023-11-05 13:29:56 -0500

    test

--- commandes/CommandeChargerImage.java
+++ commandes/CommandeChargerImage.java
@@ -1,5 +1,39 @@
 package commandes;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+
 public class CommandeChargerImage implements Commande {
-    public void executer() {  }
+    public void executer() {
+        JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        fileChooser.setDialogTitle("Sélectionnez une image");
+        fileChooser.setAcceptAllFileFilterUsed(false);
+
+        // Un filtre est établi afin de ne récupérer que des images.
+        FileNameExtensionFilter filtre = new FileNameExtensionFilter(".jpg, .png", "jpg",
+                "png");
+        fileChooser.addChoosableFileFilter(filtre);
+
+        int returnValue = fileChooser.showOpenDialog(null);
+
+        // Si le fichier choisi est une image, nous commençons à le lire. Sinon, on génère un message d'erreur.
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            try {
+                ImageIcon image = new ImageIcon(fileChooser.getSelectedFile().getAbsolutePath());
+                Image im = image.getImage();
+
+            }
+            catch (Exception e) {
+                JOptionPane.showMessageDialog(null,
+                        "Lecture impossible.",
+                        "Erreur", JOptionPane.WARNING_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    "Sélection annulée.", "Erreur",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    };
 }
--- modeles/ModeleApplication.java
+++ modeles/ModeleApplication.java
@@ -5,4 +5,10 @@ import java.util.ArrayList;
 
 public class ModeleApplication {
     ArrayList<Image> images = new ArrayList<>(3);
+
+    public void stockerNouvelleImage(Image im) {
+        for (int i = 0; i < images.size(); i++) { images.add(im); }
+    }
+
+    public ArrayList<Image> recupererImage() { return images; }
 }
