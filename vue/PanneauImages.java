e2f0d55400efd40d48a5780326f15de78fb1b00e
GenkaZero <dominic-achkarian@outlook.com>
2023-11-09 17:50:43 -0500

    test

--- vue/PanneauImages.java
+++ vue/PanneauImages.java
@@ -1,24 +1,41 @@
 package vue;
 
+import commandes.CommandePressePapier;
+import commandes.CommandeTranslation;
+import commandes.CommandeZoom;
+import controlleurs.ControlleurPerspectives;
 import modeles.ModeleApplication;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Panneau Swing qui agit en tant que conteneur des instances de l'image chargée.
  */
 public class PanneauImages extends JPanel {
+    /**** Initialisation des variables ****/
+    private final ControlleurPerspectives controlleurPerspectives;          // Controlleur des perspectives
+    private Vignette vignette;
+    private Perspective1 p1;
+    private Perspective2 p2;
 
     /**
      * Constructeur d'initialisation. Le panneau crée la vignette et les perspectives, puis les attache au modèle
      * de l'application avant de les attacher à lui-même selon un GridLayout.
      *
-     * @param ma Le modèle de l'application
+     * @param ma  Le modèle de l'application
      */
     PanneauImages(ModeleApplication ma) {
-        Vignette vignette = new Vignette(ma);
-        Perspective1 p1 = new Perspective1(ma);
-        Perspective2 p2 = new Perspective2(ma);
+        controlleurPerspectives = new ControlleurPerspectives(new CommandePressePapier(), new CommandeTranslation(),
+                new CommandeZoom(), ma);
+        vignette = new Vignette(ma);
+        p1 = new Perspective1(ma);
+        p2 = new Perspective2(ma);
+        ajouterFonctionsPerspectives();
         ma.attacherObservateur(vignette);
         ma.attacherObservateur(p1);
         ma.attacherObservateur(p2);
@@ -27,5 +44,24 @@ public class PanneauImages extends JPanel {
         add(vignette);
         add(p1);
         add(p2);
+    }
+
+    /**
+     * Ajoute aux perspectives les interactions pouvant être accomplies par l'utilisateur avec la souris.
+     */
+    private void ajouterFonctionsPerspectives() {
+        ArrayList<JPanel> perspectives = new ArrayList<>(Arrays.asList(p1, p2));
+
+        for (JPanel p : perspectives) {
+            p.addMouseListener(new MouseAdapter() {
+                public void mousePressed(MouseEvent e) {
+                    controlleurPerspectives.deplacerPerspective();
+                }
+            });
+
+            p.addMouseWheelListener((MouseWheelEvent e) -> {
+                controlleurPerspectives.agrandirPerspective();
+            });
+        }
     }
 }
